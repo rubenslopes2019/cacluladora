c3722cf33bf2138171ef1da3b3278744c6396883
Logon Aluno <logonaluno@local.com>
2019-12-14 16:32:12 -0300

    teste

--- app/src/main/java/com/br/rubenslopes/calculadoraflex/ui/login/LoginActivity.kt
+++ app/src/main/java/com/br/rubenslopes/calculadoraflex/ui/login/LoginActivity.kt
@@ -4,31 +4,64 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.br.rubenslopes.calculadoraflex.R
+import com.br.rubenslopes.calculadoraflex.ui.form.FormActivity
 import com.br.rubenslopes.calculadoraflex.ui.signup.SignUpActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
-    var NEW_USER_REQUEST = 1
+    val NEW_USER_REQUEST = 1
+
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        btSignup.setOnClickListener {
-            val criarConta = Intent(this, SignUpActivity::class.java)
+        mAuth = FirebaseAuth.getInstance()
 
-            startActivityForResult(criarConta, NEW_USER_REQUEST )
+        mAuth.currentUser?.reload() //atualizar o usuario desativado
+        if (mAuth.currentUser != null) {
+            goToHome()
         }
+
+        btLogin.setOnClickListener {
+            mAuth.signInWithEmailAndPassword(
+                inputLoginEmail.text.toString(),
+                inputLoginPassword.text.toString()
+            ).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    goToHome()
+                } else {
+                    Toast.makeText(this@LoginActivity, it.exception?.message,
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        btSignup.setOnClickListener {
+            val criarConta =
+                Intent(this, SignUpActivity::class.java)
+
+            startActivityForResult(criarConta, NEW_USER_REQUEST)
+        }
+    }
+
+    private fun goToHome() {
+        val intent = Intent(this, FormActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+        finish()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
         when(requestCode) {
             NEW_USER_REQUEST -> {
-                when(resultCode){
+                when(resultCode) {
                     Activity.RESULT_OK -> {
                         inputLoginEmail
                             .setText(data?.getStringExtra("email"))
@@ -36,7 +69,6 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
             else -> {}
-
         }
     }
 }
