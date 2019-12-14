3be9e46da3dccf9ab95d4df4f8f76a26a5ef78fe
Logon Aluno <logonaluno@local.com>
2019-12-14 12:49:08 -0300

    teste

--- app/src/main/java/com/br/rubenslopes/calculadoraflex/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/br/rubenslopes/calculadoraflex/ui/signup/SignUpActivity.kt
@@ -2,10 +2,12 @@ package com.br.rubenslopes.calculadoraflex.ui.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.br.rubenslopes.calculadoraflex.R
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
     }
 }
